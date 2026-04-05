4e1d378491b0c5d872c79d1d664a7f33d2048f6c
Ikram Marbasy <ikrammarbasy17@gmail.com>
2026-04-05 18:14:48 +0700

    My Microservices Project

---
+++ constants/error/general.go
@@ -0,0 +1,26 @@
+package error
+
+import "errors"
+
+const (
+	Success = "success"
+	Error   = "error"
+)
+
+var (
+	ErrInternalServerError = errors.New("internal server error")
+	ErrSqlError            = errors.New("database server failed to execute query")
+	ErrTooManyRequests     = errors.New("too many requests")
+	ErrUnauthorized        = errors.New("unauthorized")
+	ErrInvalidToken        = errors.New("invalid token")
+	ErrForbidden           = errors.New("forbidden")
+)
+
+var GeneralErrors = []error{
+	ErrInternalServerError,
+	ErrSqlError,
+	ErrTooManyRequests,
+	ErrUnauthorized,
+	ErrInvalidToken,
+	ErrForbidden,
+}
